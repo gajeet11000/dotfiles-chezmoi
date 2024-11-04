1f3d79d7791b4104e60c645c1008eca1f9131f6b
G Ajeet <gajeet1100@gmail.com>
2024-11-04 16:13:03 +0530

    "changed the brave backup directory to Default"

--- dot_scripts/executable_Brave_Backup.sh
+++ dot_scripts/executable_Brave_Backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-cd ~/.config/BraveSoftware/Brave-Browser/Profile\ 1/
+cd ~/.config/BraveSoftware/Brave-Browser/Default/
 
 7z a -mx7 Backup.zip Bookmarks Extension\ Rules/ Extension\ State/ Extensions/ Preferences Shortcuts
 
