d1b8151269fd025cb5548e5dc7e7eab9d25b5d95
G Ajeet <gajeet1100@gmail.com>
2024-11-06 02:29:49 +0530

    added backup datetime to show up in the log file

--- dot_scripts/executable_Brave_Backup.sh
+++ dot_scripts/executable_Brave_Backup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+echo "Backup completed on: $(date)"
+
 cd ~/.config/BraveSoftware/Brave-Browser/Default/
 
 7z a -mx7 Backup.zip Bookmarks Extension\ Rules/ Extension\ State/ Extensions/ Preferences Shortcuts
