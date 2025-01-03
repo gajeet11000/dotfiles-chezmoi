335866a631ed7edc0b205eeb861cc37c5168461c
G Ajeet <gajeet11000@gmail.com>
2025-01-04 01:07:57 +0530

    added thunar thumbnail, superfile packages and alias for lazygit

--- install.sh
+++ install.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-awk '!/^#/ && NF' packages.txt | xargs yay -S --needed
+
+# Path to the file containing the list of packages
+PACKAGE_LIST="packages.txt"
+
+# Loop through each line of the package list
+while IFS= read -r line; do
+    # Skip empty lines and lines starting with #
+    if [[ -z "$line" || "$line" == \#* ]]; then
+        continue
+    fi
+
+    # Install the package using yay
+    yay -S --noconfirm "$line"
+done < "$PACKAGE_LIST"
+
--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -7,13 +7,13 @@ set -U fish_greeting
 alias c="clear"
 alias ll="lsd -1 -hal --blocks permission,user,size,name --group-dirs=first"
 alias btop="btop --utf-force"
+alias lg="lazygit"
 
 # Package Manager
 alias search="yay -Ss "
-alias install="yay -S "
+alias install="yay -S --needed "
 alias update="yay -Syu "
 alias remove="yay -R "
-alias ai-docker-stop="cd $HOME/Documents/AiInterview/aiinterview-backend && docker-compose stop"
 
 fish_vi_key_bindings
 bind -M insert -m default ii backward-char force-repaint
