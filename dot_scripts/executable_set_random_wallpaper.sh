09c063df568b47355d21bfa3c237421dc3857a00
G Ajeet <gajeet11000@gmail.com>
2025-01-04 01:58:06 +0530

    added new wallpaper changing script

--- dot_scripts/executable_set_random_wallpaper.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-wallpapers=($(ls -d /mnt/Storage/PHOTOS/Wallpapers/* | sed 's/ /\\ /g'))
-wall=${wallpapers[ $RANDOM % ${#wallpapers[@]} ]}
-
-
-sed '1,2d' $HOME/.config/hypr/hyprpaper.conf > $HOME/.config/hypr/temp.txt
-sed -i "1i preload = $wall\nwallpaper = , $wall\n" $HOME/.config/hypr/temp.txt
-mv $HOME/.config/hypr/temp.txt $HOME/.config/hypr/hyprpaper.conf
